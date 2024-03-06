1f75d852ce9f329bbe963711e70711df1c342c6e
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-03-06 14:04:46 +0530

    Create do-while_loop.js

---
+++ do-while_loop.js
@@ -0,0 +1,17 @@
+/*
+  do-while loop
+  Synatx:
+   initialization;
+   do
+   {
+      //body of loop
+      updation;
+   }while(condition);
+*/
+
+var i=0;
+do
+  {
+    console.log(i);
+    i++;
+  }while(i<5);
