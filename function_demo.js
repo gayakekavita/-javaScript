898039a8aa5d0efd464855ac4d6be161b62f6808
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-03-06 16:10:35 +0530

    Create function_demo.js

---
+++ function_demo.js
@@ -0,0 +1,17 @@
+/*
+   Function is a block of code designed to perform a particular task.
+  */
+function add(a,b)
+{
+  return a+b;
+}
+let a=10;
+let b=20;
+console.log(add(a,b));
+
+
+const fun=()=>
+  {
+     return "hello";
+  }
+console.log(fun())
