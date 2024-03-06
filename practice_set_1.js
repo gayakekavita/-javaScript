df1c6f8177bda51c9f63ea3680fea4d057511ed1
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-03-06 12:07:15 +0530

    Create practice_set_1.js

---
+++ practice_set_1.js
@@ -0,0 +1,22 @@
+let a="harry";
+let b=89;
+console.log(a+b);
+//output of above code is harry89
+
+
+//Q2
+//check type of a+b 
+console.log(typeof a+b)
+//output of above code is string89
+
+//Q3 create a const object in javascript can you change it to hold a number later
+const obj={
+  name:'kavi',
+  age:20,
+  isStudent:true
+};
+console.log(obj)
+
+//Q4 try to add n new key to the const object 
+obj['marks']=99.99
+console.log(obj);
