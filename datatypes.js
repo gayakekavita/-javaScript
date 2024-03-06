181565dc95cffe894633a096ed296643640080b6
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-03-06 11:38:48 +0530

    Create datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+/*
+Following are the data types in javascript:
+ --Primitive data types
+ 1.null
+ 2.number
+ 3.boolean
+ 4.byte
+ 5.string
+ 6.symbol
+ 7.undefined
+
+-Non-primitive data types:
+  1. object
+
+
+*/
+
+
+let a=null;
+let num=45;
+let status=true;
+let bigint=BigInt("345")
+let str="hello";
+let symbol_var=Symbol("i am a symbol");
+let undef=undefined;
+console.log(a,num,status,bigint,str,symbol_var,undef);
