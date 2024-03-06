69d8ffd4456257d1c2d3d9495d791273737f4a54
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-03-06 13:30:14 +0530

    Create switch_statement.js

---
+++ switch_statement.js
@@ -0,0 +1,32 @@
+
+let a=2;
+switch(a)
+  {
+    case 1:
+      console.log("one");
+      break;
+    case 2:
+      console.log("two");
+      break;
+    case 3:
+      console.log("three");
+      break;
+    case 4:
+      console.log("four");
+      break;
+    case 5:
+      console.log("five");
+      break;
+    case 6:
+      console.log("six");
+      break;
+    case 7:
+      console.log("seven");
+      break;
+    case 8:
+      console.log("eight");
+      break;
+    default :
+      console.log("invalid");
+      break;
+  }
