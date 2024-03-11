e01c03ef4b54d31fe3e1ccc419cb9d4fa30c1aeb
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-03-11 10:33:24 +0530

    Update set_get_hasAttribute.js

--- set_get_hasAttribute.js
+++ set_get_hasAttribute.js
@@ -1,3 +1,27 @@
+/*
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  
+    <div class="firstclass" id="first" data-game="mario" data-player="kauu">
+      Hello Everyone
+    </div>
+        
+  
+  <script src="script.js"></script>
+</body>
+
+</html>
+*/
 let first1=document.getElementById("first");
 console.log("has attribute of style or not: "+ first1.hasAttribute("style"));
 console.log("Setting attribute of style: "+ first1.setAttribute("style","color:red"));
@@ -7,3 +31,17 @@ console.log(""+ first1.nodeValue);
 console.log("getting dataset"+ first1.dataset);
 console.log("Game is: "+ first1.dataset.game);
 console.log("Player is:" + first1.dataset.player);
+console.log(first1.getAttribute("data-game"));
+/*
+output:::
+has attribute of style or not: false
+devtools.js:109 Setting attribute of style: undefined
+devtools.js:109 attributes of first1 id: [object NamedNodeMap]
+devtools.js:109 tag name: DIV
+devtools.js:109 null
+devtools.js:109 getting dataset[object DOMStringMap]
+devtools.js:109 Game is: mario
+devtools.js:109 Player is:kauu
+devtools.js:109 mario
+
+*/
