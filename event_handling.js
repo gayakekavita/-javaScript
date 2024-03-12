7c66b994e5eebbb6201ef35c91b607bc249fc6dd
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-03-12 14:14:41 +0530

    Update event_handling.js

--- event_handling.js
+++ event_handling.js
@@ -1,2 +1,9 @@
 //in html document event handling
 <button onclick="alert('hello')">Click me</button>
+
+
+//js
+let first=document.getElementById("first");
+first.addEventListener('click', alert('hello'));
+
+first.removeEventListener('click',alert('removed event'));
