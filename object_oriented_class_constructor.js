5abff57be2f4c7c1552ada3e69fefe8e406e7528
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-03-15 19:05:20 +0530

    Create object_oriented_class_constructor.js

---
+++ object_oriented_class_constructor.js
@@ -0,0 +1,15 @@
+
+//creating a class with the name of 'hello'
+class hello{
+  
+  constructor(name,roll) //parameterized constructor 
+  {
+      console.log("Name is : "+ name+ " and roll is : "+ roll);
+  }
+}
+//creating object of hello class as 'h1' using following code of lines 
+let h1=new hello('kavi',20);
+
+//when object of a class is created then constructor is called.
+//if we do not provide any constructor then default constructor is provided by javascript.
+
