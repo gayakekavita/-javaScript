3fd51ec8e8cb83e10c5edc334ea197da2e2c986d
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-03-15 19:00:56 +0530

    Create object_orinted_classMethodwithparameter.js

---
+++ object_orinted_classMethodwithparameter.js
@@ -0,0 +1,14 @@
+
+//creating a class with the name of 'hello'
+class hello{
+  
+  say(name,roll)
+  {
+    
+      console.log("name is : "+ name +" and roll is :  "+roll);
+  }
+}
+//creating object of hello class as 'h1' using following code of lines 
+let h1=new hello();
+//calling to the method of class hello 'say'
+h1.say("kavi",20); //passing parameter to say method
