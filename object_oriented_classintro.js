e3e6375dead57823bb7bf481d55120542001104c
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-03-15 18:56:44 +0530

    Create object_oriented_classintro.js

---
+++ object_oriented_classintro.js
@@ -0,0 +1,12 @@
+
+//creating a class with the name of 'hello'
+class hello{
+  say()
+  {
+    alert("hello");
+  }
+}
+//creating object of hello class as 'h1' using following code of lines 
+let h1=new hello();
+//calling to the method of class hello 'say'
+h1.say();
